ba1d048ad2bcfa7b859167b54406ff58a2118118
Ferran Pelayo Monfort <ferran91@gmail.com>
2013-06-23 20:37:05 +0200

    El horario se obtiene en una tabla

--- renfe/renfe.py
+++ renfe/renfe.py
@@ -1,6 +1,6 @@
-import urllib
 import sqlite3
 import time
+import urllib
 from bs4 import BeautifulSoup
 
 class Renfe(object):
@@ -38,39 +38,50 @@ class Renfe(object):
     p = urllib.urlopen(url, params)
     return p.read()
 
-  def parse_page(self, table):
+  def parse_table(self, table):
     list = []
     trs = self.get_trs(table)
     for tr in trs:
       tds = self.get_tds(tr)
       obj = []
       for td in tds:
-        obj.append(td.string)
+        td = td.string
+        if td:
+          td = td.strip()
+        obj.append(td)
       list.append(obj)
-    return list
-
-  def parse_transbordo(self, table):
-    list = []
-    trs = self.get_trs(table)
-    tds = self.get_tds(trs)
-    print tds
     return list
 
   def get_trs(self, table):
     trs = table.tr.td.find_all('tr') # devuelve un tr con muchos trs dentro
-    # del trs[0] # borrar primer td (donde esta la info)
     return trs
 
   def get_tds(self, tr):
     tds = tr.find_all('td')
     return tds
 
+  def parse_table_transbordo(self, list):
+    pass
+    # Borramos las entradasa que no interesan
+    # list.pop(0)
+    # list.pop(0)
+    # list.pop(0)
+  
+  def parse_table_normal(self, list):
+    list.pop(0)
+
+  def hay_transbordo(self, list):
+    return len(list[0]) > 5
+
   def parse_page(self, html):
-    # print html
     html = BeautifulSoup(html)
     table = BeautifulSoup(str(html.table))
     list = []
-    list = self.parse_page(table)
+    list = self.parse_table(table)
+    if self.hay_transbordo(list):
+      self.parse_table_transbordo(list)
+    else:
+      self.parse_table_normal(list)
     return list
 
   def get_horarios(self, orig, dest):
@@ -110,7 +121,7 @@ if __name__ == '__main__':
   horarios = r.get_horarios(origen, destino)
   if horarios:
     for horario in horarios:
-      print horario
+      print horarios.index(horario), horario
       pass
   else:
     print 'No he conseguido los horarios'
