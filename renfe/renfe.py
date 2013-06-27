b19062eec8b75819aef75d6eb4c5f2ef0f429a0c
Ferran Pelayo Monfort <ferran91@gmail.com>
2013-06-27 18:25:46 +0200

    Creadas clases para mejor mantenimiento de los horarios

--- renfe/renfe.py
+++ renfe/renfe.py
@@ -1,42 +1,121 @@
 import sqlite3
 import time
 import urllib
+import json
 from bs4 import BeautifulSoup
 
-class Renfe(object):
-  def __init__(self):
-    self.conn = sqlite3.connect('database')
+class Ciudad:
+  def __init__(self, nucleo, nombre):
+    self.nucleo = nucleo
+    self.nombre = nombre
 
-  def get_ciudades(self):
-    cursor = self.conn.cursor()
-    cursor.execute('SELECT nucleo, ciudad FROM ciudades ORDER BY ciudad ASC')
-    return cursor.fetchall()
+  def to_dict(self):
+    return {
+        'nucleo': self.nucleo,
+        'nombre': self.nombre
+        }
 
-  def get_estaciones(self, nucleo):
-    cursor = self.conn.cursor()
-    cursor.execute('SELECT id, estacion FROM estaciones WHERE nucleo=%s ORDER BY estacion ASC' % nucleo)
-    return cursor.fetchall()
+  @staticmethod
+  def get_ciudades():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT nucleo, nombre FROM ciudades ORDER BY nombre ASC;')
+    list = []
+    for ciudad in cursor.fetchall():
+      list.append(Ciudad(ciudad[0], str(ciudad[1])))
+    connection.commit()
+    connection.close()
+    return list
 
-  def get_nucleo(self, estacion):
-    cursor = self.conn.cursor()
-    cursor.execute('SELECT c.nucleo FROM ciudades c, estaciones e WHERE c.nucleo=e.nucleo AND e.id=%s' % estacion)
-    return cursor.fetchone()[0]
+class Estacion:
+  def __init__(self, id, nombre, nucleo):
+    self.nombre = nombre
+    self.id = id
+    self.nucleo = nucleo
 
-  def get_page(self, nucleo, orig, dest, date=time.strftime("%Y%m%d"), ho=00, hd=26):
+  def to_dict(self):
+    return {
+        'id': self.id,
+        'nombre': self.nombre
+        }
+
+  @staticmethod
+  def get_estaciones(nucleo):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT id, nombre FROM estaciones WHERE nucleo=%s ORDER BY nombre ASC' % str(nucleo))
+    list = []
+    for estacion in cursor.fetchall():
+      list.append(Estacion(estacion[0], estacion[1], nucleo))
+    connection.commit()
+    connection.close()
+    return list
+    
+class Transbordo:
+  def __init__(self, estacion, hl, hs):
+    self.estacion = estacion
+    self.hl = hl
+    self.hs = hs
+
+  def to_dict(self):
+    return {
+        'estacion': estacion.to_dict(),
+        'hl': self.hl,
+        'hs': self.hs
+        }
+
+class Trayecto:
+  def __init__(self, linea, ho, hd, tiempo, transbordo=None):
+    self.linea = linea
+    self.ho = ho
+    self.hd = hd
+    self.tiempo = tiempo
+    self.transbordo = transbordo
+
+  def to_dict(self):
+    return {
+        'linea': self.linea,
+        'ho': self.ho,
+        'hd': self.hd,
+        'tiempo': self.tiempo,
+        'transbordo': self.transbordo.to_dict if self.transbordo else None
+        }
+
+class Horario:
+  def __init__(self, origen, destino, ho=00, hd=26, date=time.strftime("%Y%m%d")):
+    self.origen = origen
+    self.destino = destino
+    self.ho = ho
+    self.hd = hd
+    self.date = date
+    self.table = self.get_horario()
+
+  def get_page(self):
     url = 'http://horarios.renfe.com/cer/hjcer310.jsp'
     params = urllib.urlencode({
       'f1': '',
       'i': 's',
       'cp': 'NO',
-      'nucleo': nucleo, # nucleo
-      'o': orig, # estacion origen
-      'd': dest, # estacion destino
-      'df': date, # dia en formato yyyymmdd
-      'ho': ho, # hora de origen
-      'hd': hd, # hora de destino
+      'nucleo': self.origen.nucleo, # nucleo
+      'o': self.origen.id, # estacion origen
+      'd': self.destino.id, # estacion destino
+      'df': self.date, # dia en formato yyyymmdd
+      'ho': self.ho, # hora de origen
+      'hd': self.hd, # hora de destino
       'TXTInfo': ''})
     p = urllib.urlopen(url, params)
     return p.read()
+
+  def get_trs(self, table):
+    trs = table.tr.td.find_all('tr') # devuelve un tr con muchos trs dentro
+    return trs
+
+  def get_tds(self, tr):
+    tds = tr.find_all('td')
+    return tds
+
+  def hay_transbordo(self, list):
+    return len(list[0]) > 5
 
   def parse_table(self, table):
     list = []
@@ -52,13 +131,13 @@ class Renfe(object):
       list.append(obj)
     return list
 
-  def get_trs(self, table):
-    trs = table.tr.td.find_all('tr') # devuelve un tr con muchos trs dentro
-    return trs
-
-  def get_tds(self, tr):
-    tds = tr.find_all('td')
-    return tds
+  def parse_table_normal(self, list):
+    list.pop(0)
+    l = []
+    for row in list:
+      t = Trayecto(str(row[0]), str(row[1]), str(row[2]), str(row[3]))
+      l.append(t)
+    return l
 
   def parse_table_transbordo(self, list):
     pass
@@ -67,29 +146,30 @@ class Renfe(object):
     # list.pop(0)
     # list.pop(0)
   
-  def parse_table_normal(self, list):
-    list.pop(0)
-
-  def hay_transbordo(self, list):
-    return len(list[0]) > 5
-
   def parse_page(self, html):
     html = BeautifulSoup(html)
     table = BeautifulSoup(str(html.table))
     list = []
     list = self.parse_table(table)
     if self.hay_transbordo(list):
-      self.parse_table_transbordo(list)
+      list = self.parse_table_transbordo(list)
     else:
-      self.parse_table_normal(list)
+      list = self.parse_table_normal(list)
     return list
 
-  def get_horarios(self, orig, dest):
-    nucleo = self.get_nucleo(orig)
-    oelcun = self.get_nucleo(dest)
+  def get_horario(self):
+    nucleo = self.origen.nucleo
+    oelcun = self.destino.nucleo
     if nucleo == oelcun:
-      page = self.get_page(nucleo, orig, dest)
+      page = self.get_page()
       return self.parse_page(page)
+
+  def to_dict(self):
+    return {
+        'origen': self.origen.to_dict(),
+        'destino': self.destino.to_dict(),
+        'horarios': [l.to_dict() for l in self.table]
+        }
 
 
 
@@ -98,30 +178,8 @@ class Renfe(object):
 ##################################################
 
 if __name__ == '__main__':
-  r = Renfe()
-  ciudades = r.get_ciudades()
-  i = 0
-  for ciudad in ciudades:
-    # print i, ciudad
-    i += 1
-  # n = int(input('Select Ciudad: '))
-  n = 1
-  estaciones = r.get_estaciones(ciudades[n][0])
-  i = 0
-  for estacion in estaciones:
-    # print i, estacion
-    i += 1
-  # n = int(input('Select Origen: '))
-  n = 96 # sanfe
-  origen = estaciones[n][0]
-  # n = int(input('Select Destino: '))
-  # n = 97 # sant joan
-  n = 0 # aeroport
-  destino = estaciones[n][0]
-  horarios = r.get_horarios(origen, destino)
-  if horarios:
-    for horario in horarios:
-      print horarios.index(horario), horario
-      pass
-  else:
-    print 'No he conseguido los horarios'
+  l = Estacion.get_estaciones(50)
+  o = l[96]
+  d = l[97]
+  h = Horario(o, d).to_dict()
+  print h
