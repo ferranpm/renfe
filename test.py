f0f5e247388c978789f895fa1c8d8d56a2df0d65
Ferran Pelayo Monfort <ferran91@gmail.com>
2013-07-14 13:46:39 +0200

    Añadido un test

---
+++ test.py
@@ -0,0 +1,10 @@
+from renfe import *
+import os
+
+if __name__ == '__main__':
+  l = Estacion.get_estaciones(50)
+  o = l[96] # para probar con un trayecto simple
+  # o = l[0] # para probar con un transbordo
+  d = l[97]
+  h = Horario(o, d)
+  print h.to_dict()
