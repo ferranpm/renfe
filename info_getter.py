126c2481e5fa3a26a51a00420d101fb0bd5a841a
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2013-12-13 23:52:47 +0100

    simpler functions, better life

--- info_getter.py
+++ info_getter.py
@@ -2,7 +2,7 @@ import urllib2
 from xml.dom import minidom
 
 RENFE_NUCLEOS = 'http://renfe.mobi/renfev2/ciudades_cercanias.do'
-RENFE_ESTACIONES = 'http://renfe.mobi/renfev2/hora_ga_cercanias.do?'
+RENFE_ESTACIONES = 'http://renfe.mobi/renfev2/hora_ga_cercanias.do'
 
 def get_list_nucleo_ciudad():
     page = urllib2.urlopen(RENFE_NUCLEOS)
@@ -22,37 +22,29 @@ def get_list_nucleo_ciudad():
                 })
     return lnc
 
-def get_list_nucleo_estacion(list_nucleo_ciudad):
-    lne = []
-    for nc in list_nucleo_ciudad:
-        nucleo, ciudad = nc['nucleo'], nc['ciudad']
-        url = RENFE_ESTACIONES + "nucleo=%s&ciudad=%s" % (nucleo, ciudad)
-        page = urllib2.urlopen(url)
-        content = page.read()
-        dom = minidom.parseString(content)
-        selects = dom.getElementsByTagName('select')
-        le = []
-        for select in selects:
-            if select.attributes['name'].nodeValue == 'o':
-                for e in select.childNodes:
-                    id_estacion = e.attributes['value'].nodeValue.encode('utf-8')
-                    estacion = e.firstChild.nodeValue.encode('utf-8')
-                    le.append({
-                        'id': id_estacion,
-                        'estacion': estacion
-                        })
-        lne.append({
-            'ciudad': ciudad,
-            'nucleo': nucleo,
-            'estaciones': le
-            })
-    return lne
+def get_list_id_estacion(nucleo, ciudad):
+    url = RENFE_ESTACIONES + "?nucleo=%s&ciudad=%s" % (nucleo, ciudad)
+    page = urllib2.urlopen(url)
+    content = page.read()
+    dom = minidom.parseString(content)
+    selects = dom.getElementsByTagName('select')
+    le = []
+    for select in selects:
+        if select.attributes['name'].nodeValue == 'o':
+            for e in select.childNodes:
+                id_estacion = e.attributes['value'].nodeValue.encode('utf-8')
+                estacion = e.firstChild.nodeValue.encode('utf-8')
+                le.append({
+                    'id': id_estacion,
+                    'estacion': estacion
+                    })
+    return le
 
 if __name__ == '__main__':
     lnc = get_list_nucleo_ciudad()
-    lne = get_list_nucleo_estacion(lnc)
-    for c in lne:
-        print c['nucleo'], c['ciudad']
-        for e in c['estaciones']:
+    for nc in lnc:
+        print nc['nucleo'], nc['ciudad']
+        le = get_list_id_estacion(nc['nucleo'], nc['ciudad'])
+        for e in le:
             print e['id'], e['estacion']
         print
