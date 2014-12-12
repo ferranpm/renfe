f2041c6eb74caac93fd934070eb7dd767eb5f360
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2014-12-12 01:17:24 +0100

    Add ruby method

---
+++ renfe.rb
@@ -0,0 +1,132 @@
+require 'nokogiri'
+require 'sqlite3'
+require 'net/http'
+
+$db = SQLite3::Database.new('database')
+
+class Referentiable
+    attr_reader :id, :nombre
+    def initialize id, nombre
+        @id, @nombre = id, nombre
+    end
+
+    def dict
+        {
+            :id => @id,
+            :nombre => @nombre
+        }
+    end
+end
+
+class Nucleo < Referentiable
+    def self.get_by_id id
+        row = $db.execute("SELECT nombre FROM nucleos WHERE id=#{id};")
+        Nucleo.new(id, row[0])
+    end
+
+    def self.get_nucleos
+        nucleos = []
+        $db.execute("SELECT id, nombre FROM nucleos") do |row|
+            nucleos.push(Nucleo.new(row[0], row[1]))
+        end
+        nucleos
+    end
+end
+
+class Estacion < Referentiable
+    def nucleo
+        row = $db.get_first_row("SELECT n.id, n.nombre FROM nucleos n, estaciones e WHERE e.id=#{id} AND e.nucleo_id=n.id;")
+        Nucleo.new(row[0], row[1])
+    end
+
+    def self.get_by_id id
+        row = $db.execute("SELECT nombre FROM estaciones WHERE id=#{id};")
+        Estacion.new(id, row[0])
+    end
+
+    def self.get_estaciones nucleo
+        estaciones = []
+        $db.execute("SELECT id, nombre FROM estaciones WHERE nucleo_id=#{nucleo.id} ORDER BY nombre ASC;") do |row|
+            estaciones.push(Estacion.new(row[0], row[1]))
+        end
+        estaciones
+    end
+end
+
+class ItinerarioSimple
+    attr_accessor :linea, :ho, :hd
+    def initialize linea, ho, hd
+        @linea, @ho, @hd = linea, ho, hd
+    end
+end
+
+class ItinerarioDoble
+    attr_accessor :linea1, :ho1, :hd1, :linea2, :ho2, :hd2
+    def initialize linea1, ho1, hd1, linea2, ho2, hd2
+        @linea1, @ho1, @hd1, @linea2, @ho2, @hd2 = linea1, ho1, hd1, linea2, ho2, hd2
+    end
+end
+
+class Horario
+    attr_accessor :origen, :destino, :ho, :hd, :date
+    attr_accessor :horas, :transbordo
+    def initialize origen, destino, ho=00, hd=26, date="20141211"
+        @origen, @destino, @ho, @hd, @date = origen, destino, ho, hd, date
+        @horas = []
+        @transbordo = false
+        parse_page get_page
+    end
+
+    def get_page
+        url = "http://horarios.renfe.com"
+        params = "/cer/hjcer310.jsp?&f1=&df=#{date}&TXTInfo=&hd=#{hd}&d=#{destino.id}&i=s&cp=NO&nucleo=#{origen.nucleo.id}&o=#{origen.id}&ho=#{ho}"
+
+        Nokogiri::HTML(Net::HTTP.get_response(URI.parse(url + params)).body)
+    end
+
+    def parse_sin_transbordo table
+        table.xpath('//tr')[2..-1].each do |tr|
+            td_list = tr.xpath('td').children
+            linea = td_list[0] # linea
+            horig = td_list[1] # hd
+            hdest = td_list[2] # ho
+            time  = td_list[3] # time
+            @horas << ItinerarioSimple.new(linea, horig, hdest)
+        end
+    end
+
+    def parse_con_transbordo table
+        prev = nil
+        table.xpath('//tr')[4..-1].each do |tr|
+            td_list = tr.xpath('td').children
+            linea1, horig1, hdest1 = nil
+            if td_list[0].text.strip == "" and prev
+                linea1 = prev[0].text
+                horig1 = prev[1].text
+                hdest1 = prev[2].text
+            else
+                linea1 = td_list[0].text
+                horig1 = td_list[1].text
+                hdest1 = td_list[2].text
+            end
+            linea2 = td_list[4].text
+            horig2 = td_list[3].text
+            hdest2 = td_list[5].text
+            time = td_list[6].text
+            @horas << ItinerarioDoble.new(linea1, horig1, hdest1, linea2, horig2, hdest2)
+            prev = td_list if not prev
+        end
+    end
+
+    def parse_page page
+        table = page.xpath('//table')[0]
+        @transbordo = table.xpath('//tr')[1].children.size > 11
+        if transbordo
+            parse_con_transbordo table
+        else
+            parse_sin_transbordo table
+        end
+    end
+
+    private :get_page, :parse_sin_transbordo, :parse_con_transbordo, :parse_page
+end
