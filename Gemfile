b5181f497c8f8ac974ad9958de9cd0e726172c64
Ferran Pelayo Monfort <ferran.pelayo@sequra.es>
2017-12-09 23:59:53 +0100

    [ADD] Simple server

--- Gemfile
+++ Gemfile
@@ -2,6 +2,11 @@ source "https://rubygems.org"
 
 ruby "2.4.1"
 
-gem "nokogiri", "~> 1.8"
-gem "sqlite3", "~> 1.3"
 gem "attr_extras", "~> 5.2"
+gem "nokogiri", "~> 1.8"
+gem "sinatra", "~> 2.0"
+gem "sqlite3", "~> 1.3"
+
+group :development do
+  gem "sinatra-contrib", "~> 2.0"
+end
---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'sinatra/json'
+require_relative './lib/renfe.rb'
+
+if development?
+  require 'sinatra/reloader'
+end
+
+get '/nucleos' do
+  erb :nucleos, locals: { nucleos: Nucleo.all }
+end
+
+get '/estaciones/:id' do |id|
+  erb :estaciones, locals: { nucleo: Nucleo.find(id) }
+end
+
+get '/horario' do
+  origen = Estacion.find(params[:origen].to_i)
+  destino = Estacion.find(params[:destino].to_i)
+  horario = Horario.new(origen, destino)
+  template = horario.transbordo? ? :transbordo : :simple
+  erb template, locals: { horario: horario }
+end
