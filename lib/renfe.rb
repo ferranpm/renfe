30d139d07a2b5fed65709076dac49d92857d483c
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2024-03-05 21:53:46 +0100

    Fix some Rubocop complaints

--- lib/renfe.rb
+++ lib/renfe.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'nokogiri'
-require 'open-uri'
-require_relative './renfe/version'
+require "nokogiri"
+require "open-uri"
+require_relative "renfe/version"
 
 module Renfe
   class Nucleo
@@ -26,7 +26,7 @@ module Renfe
       { id: 61, nombre: "San Sebastián" },
       { id: 62, nombre: "Santander" },
       { id: 70, nombre: "Zaragoza" },
-    ]
+    ].freeze
 
     def self.all
       NUCLEOS.map do |nucleo|
@@ -64,7 +64,7 @@ module Renfe
       @date = date
     end
 
-    attr_reader :origen, :destino, :hora_inicio, :hora_fin, :date
+    attr_reader :origen, :destino
 
     def horas
       @horas ||= parse_page
@@ -97,36 +97,43 @@ module Renfe
     end
 
     def transbordo?
-      table.css('tr')[0].css('td').size > 5
+      table.at_css("tr").css("td").size > 5
     end
 
     def parse_con_transbordo
       prev = nil
-      table.css('tr')[4..-1].map do |tr|
-        row = tr.css('td').map(&:text).map(&:strip)
-        linea_1        = presence(row[0]) || prev && prev[0]
-        hora_inicio_1  = presence(row[2]) || prev && prev[2]
-        hora_fin_1     = presence(row[3]) || prev && prev[3]
-        linea_2        = presence(row[5]) || prev && prev[5]
-        hora_inicio_2  = presence(row[4]) || prev && prev[4]
-        hora_fin_2     = presence(row[7]) || prev && prev[7]
-        prev = row
-        ItinerarioDoble.new(linea_1, hora_inicio_1, hora_fin_1, linea_2, hora_inicio_2, hora_fin_2)
+      table.css("tr")[4..].map do |tr|
+        row = tr.css("td").map(&:text).map(&:strip)
+
+        build_itinerario_doble(row, prev)
       end
+    end
+
+    def build_itinerario_doble(row, prev)
+      ItinerarioDoble.new(
+        row[0] || prev[0],
+        row[2] || prev[2],
+        row[3] || prev[3],
+        row[5] || prev[5],
+        row[4] || prev[4],
+        row[7] || prev[7],
+      )
     end
 
     def parse_sin_transbordo
-      table.css('tr')[1..-1].map do |tr|
-        row = tr.css('td').map(&:text).map(&:strip)
-        linea = row[0]
-        hora_inicio = row[2]
-        hora_fin = row[3]
-        ItinerarioSimple.new(linea, hora_inicio, hora_fin)
+      table.css("tr")[1..].map do |tr|
+        row = tr.css("td").map(&:text).map(&:strip)
+
+        build_itinerario_simple(row)
       end
     end
 
+    def build_itinerario_simple(row)
+      ItinerarioSimple.new(row[0], row[2], row[3])
+    end
+
     def table
-      page.css('table')[0]
+      page.at_css("table#tabla")
     end
 
     def page
@@ -134,15 +141,20 @@ module Renfe
     end
 
     def uri
-      URI.parse("https://horarios.renfe.com/#{params}")
+      uri = URI.parse("https://horarios.renfe.com/cer/hjcer310.jsp")
+      uri.query = URI.encode_www_form(params)
+      uri
     end
 
     def params
-      "cer/hjcer310.jsp?&f1=&df=#{date.strftime("%Y%m%d")}&TXTInfo=&hd=#{hora_fin}&d=#{destino.id}&i=s&cp=NO&nucleo=#{origen.nucleo.id}&o=#{origen.id}&ho=#{hora_inicio}"
-    end
-
-    def presence(item)
-      item.nil? || item.empty? ? nil : item
+      {
+        "f1" => "",
+        "TXTInfo" => "",
+        "i" => "s",
+        "cp" => "NO",
+        "nucleo" => origen.nucleo.id, "o" => origen.id, "d" => destino.id,
+        "df" => date.strftime("%Y%m%d"), "ho" => hora_inicio, "hd" => hora_fin,
+      }
     end
   end
 
